351d17243413dd9dc884f7783edb9cc9d22b200e
Salvialf <jeuxalf@live.fr>
2022-01-06 15:49:12 +0100

    some UI

--- desktop/js/enedis.js
+++ desktop/js/enedis.js
@@ -7,7 +7,7 @@
 * (at your option) any later version.
 *
 * Jeedom is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* but WITHOUT ANY WARRANTY without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 * GNU General Public License for more details.
 *
@@ -16,72 +16,71 @@
 */
 
 $('.eqLogicAttr[data-l2key=widgetTemplate]').on('change', function() {
-  if( $(this).is(':checked') ){
-    $('#templateParams').show();
-  } else {
-    $('#templateParams').hide();
+  if( $(this).is(':checked')){
+    $('#templateParams').show()
+  }
+  else {
+    $('#templateParams').hide()
   }
 })
 
-$("#table_cmd").sortable({axis: "y", cursor: "move", items: ".cmd", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
+$("#table_cmd").sortable({axis: "y", cursor: "move", items: ".cmd", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true})
 function addCmdToTable(_cmd) {
   if (!isset(_cmd)) {
-    var _cmd = {configuration: {}};
+    var _cmd = {configuration: {}}
   }
   if (!isset(_cmd.configuration)) {
-    _cmd.configuration = {};
+    _cmd.configuration = {}
   }
-  var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">';
-  tr += '<td style="min-width:50px;width:70px;">';
-  tr += '<span class="cmdAttr" data-l1key="id"></span>';
-  tr += '</td>';
-  tr += '<td style="min-width:250px;width:330px;">';
-  tr += '<div class="row">';
-  tr += '<div class="col-xs-7">';
-  tr += '<input class="cmdAttr form-control input-sm" data-l1key="name" placeholder="{{Nom de la commande}}">';
-  tr += '</div>';
-  tr += '<div class="col-xs-5">';
-  tr += '<a class="cmdAction btn btn-default btn-sm" data-l1key="chooseIcon"><i class="fas fa-flag"></i> {{Icône}}</a>';
-  tr += '<span class="cmdAttr" data-l1key="display" data-l2key="icon" style="margin-left : 10px;"></span>';
-  tr += '</div>';
-  tr += '</div>';
-  tr += '</td>';
-  tr += '<td style="min-width:120px;width:140px;">';
-  tr += '<span class="type" type="' + init(_cmd.type) + '">' + jeedom.cmd.availableType() + '</span>';
-  tr += '<span class="subType" subType="' + init(_cmd.subType) + '"></span>';
-  tr += '</td>';
-  tr += '<td>';
-  tr += '<input class="cmdAttr form-control input-sm" data-l1key="unite" placeholder="{{Unité}}" title="{{Unité}}" style="min-width:60px;max-width:100px;"/>';
-  tr += '</td>';
-  tr += '<td style="min-width:260px;width:300px;">';
-  tr += '<label class="checkbox-inline"><input type="checkbox" class="cmdAttr" data-l1key="isVisible" checked/>{{Afficher}}</label>';
-  tr += '<label class="checkbox-inline"><input type="checkbox" class="cmdAttr" data-l1key="isHistorized" checked/>{{Historiser}}</label>';
-  tr += '<label class="checkbox-inline"><input type="checkbox" class="cmdAttr" data-l1key="display" data-l2key="invertBinary"/>{{Inverser}}</label>';
-  tr += '</td>';
-  tr += '<td style="max-width:260px;width:300px;">';
+  var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">'
+  tr += '<td style="min-width:50px;width:70px;">'
+  tr += '<span class="cmdAttr" data-l1key="id"></span>'
+  tr += '</td>'
+  tr += '<td style="min-width:250px;width:330px;">'
+  tr += '<div class="input-group">'
+  tr += '<input class="cmdAttr form-control input-sm roundedLeft" data-l1key="name" placeholder="{{Nom de la commande}}">'
+  tr += '<span class="input-group-btn">'
+  tr += '<a class="cmdAction btn btn-sm btn-default" data-l1key="chooseIcon" title="{{Choisir une icône}}"><i class="fas fa-icons"></i></a>'
+  tr += '</span>'
+  tr += '<span class="cmdAttr input-group-addon roundedRight" data-l1key="display" data-l2key="icon" style="font-size:19px;padding:0 5px 0 0!important;"></span>'
+  tr += '</div>'
+  tr += '</td>'
+  tr += '<td style="min-width:120px;width:140px;">'
+  tr += '<span class="type" type="' + init(_cmd.type) + '">' + jeedom.cmd.availableType() + '</span>'
+  tr += '<span class="subType" subType="' + init(_cmd.subType) + '"></span>'
+  tr += '</td>'
+  tr += '<td>'
+  tr += '<input class="cmdAttr form-control input-sm" data-l1key="unite" placeholder="{{Unité}}" title="{{Unité}}" style="min-width:60px;max-width:100px;">'
+  tr += '</td>'
+  tr += '<td style="min-width:260px;width:300px;">'
+  tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isVisible" checked>{{Afficher}}</label></span> '
+  tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isHistorized" checked>{{Historiser}}</label></span> '
+  tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="display" data-l2key="invertBinary">{{Inverser}}</label></span> '
+  tr += '</td>'
+  tr += '<td style="max-width:260px;width:300px;">'
   if (is_numeric(_cmd.id)) {
-    tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fas fa-cogs"></i></a> ';
-    tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> Tester</a>';
+    tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fas fa-cogs"></i></a> '
+    tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fas fa-rss"></i> Tester</a>'
     if ((_cmd.logicalId).includes('daily') || (_cmd.logicalId).includes('load')) {
-      tr += ' <a class="btn btn-primary btn-xs cmdAction" data-action="addEnedisData" data-logicalId="'+_cmd.logicalId+'"><i class="fas fa-calendar-plus"></i> {{Ajout historiques}}</a>';
+      tr += ' <a class="btn btn-primary btn-xs cmdAction" data-action="addEnedisData" data-logicalId="'+_cmd.logicalId+'"><i class="fas fa-calendar-plus"></i> {{Ajout historiques}}</a>'
     }
   }
-  tr += '<i class="fas fa-minus-circle pull-right cmdAction cursor" data-action="remove"></i></td>';
-  tr += '</tr>';
-  $('#table_cmd tbody').append(tr);
-  var tr = $('#table_cmd tbody tr').last();
+  tr += '<i class="fas fa-minus-circle pull-right cmdAction cursor" data-action="remove"></i></td>'
+  tr += '</tr>'
+  $('#table_cmd tbody').append(tr)
+  var tr = $('#table_cmd tbody tr').last()
   jeedom.eqLogic.builSelectCmd({
     id:  $('.eqLogicAttr[data-l1key=id]').value(),
     filter: {type: 'info'},
     error: function (error) {
-      $('#div_alert').showAlert({message: error.message, level: 'danger'});
+      $('#div_alert').showAlert({message: error.message, level: 'danger'})
     },
     success: function (result) {
-      tr.find('.cmdAttr[data-l1key=value]').append(result);
-      tr.setValues(_cmd, '.cmdAttr');
-      jeedom.cmd.changeType(tr, init(_cmd.subType));
+      tr.find('.cmdAttr[data-l1key=value]').append(result)
+      tr.setValues(_cmd, '.cmdAttr')
+      jeedom.cmd.changeType(tr, init(_cmd.subType))
     }
-  });
+  })
 
   $('.cmdAction[data-action=addEnedisData]').off().on('click', function() {
     var tr = $(this).closest('tr')
@@ -93,8 +92,8 @@ function addCmdToTable(_cmd) {
     }
     else {
       var message = '<p>{{Intégrer les consommations horaires jusqu\'à 7 jours après la date choisie}}</p>'
-      var loadDate = d.getDate() - 7;
-      d.setDate(loadDate);
+      var loadDate = d.getDate() - 7
+      d.setDate(loadDate)
       var max = d.getFullYear()+'-'+("0" + (d.getMonth() + 1)).slice(-2)+'-'+("0" + d.getDate()).slice(-2)
     }
     message += '{{Choisir la date de début}}'
@@ -107,7 +106,7 @@ function addCmdToTable(_cmd) {
       size: 'small',
       callback: function (result) {
         if (result) {
-          $('#div_alert').showAlert({message: '{{En cours d\'intégration d\'historiques...}}', level: 'warning'});
+          $('#div_alert').showAlert({message: '{{En cours d\'intégration d\'historiques...}}', level: 'warning'})
           $.ajax({
             type: "POST",
             url: "plugins/enedis/core/ajax/enedis.ajax.php",
@@ -118,10 +117,10 @@ function addCmdToTable(_cmd) {
             },
             dataType: 'json',
             error: function (request, status, error) {
-              handleAjaxError(request, status, error, $('#div_alert'));
+              handleAjaxError(request, status, error, $('#div_alert'))
             },
             success: function (data) {
-              $('#div_alert').showAlert({message: '{{Historiques intégrés avec succès}}', level: 'success'});
+              $('#div_alert').showAlert({message: '{{Historiques intégrés avec succès}}', level: 'success'})
             }
           })
         }
--- desktop/php/enedis.php
+++ desktop/php/enedis.php
@@ -25,10 +25,11 @@ $eqLogics = eqLogic::byType($plugin->getId());
 		<legend><i class="fas fa-charging-station"></i> {{Mes compteurs}}</legend>
 		<?php
 		if (count($eqLogics) == 0) {
-			echo '<br/><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun compteur Enedis n\'est paramétré, cliquer sur "Ajouter" pour commencer}}</div>';
-		} else {
+			echo '<br><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun compteur Enedis n\'est paramétré, cliquer sur "Ajouter" pour commencer}}</div>';
+		}
+		else {
 			echo '<div class="input-group" style="margin:5px;">';
-			echo '<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic"/>';
+			echo '<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic">';
 			echo '<div class="input-group-btn">';
 			echo '<a id="bt_resetSearch" class="btn" style="width:30px"><i class="fas fa-times"></i></a>';
 			echo '<a class="btn roundedRight hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>';
@@ -38,9 +39,12 @@ $eqLogics = eqLogic::byType($plugin->getId());
 			foreach ($eqLogics as $eqLogic) {
 				$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
 				echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
-				echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+				echo '<img src="' . $plugin->getPathImgIcon() . '">';
 				echo '<br>';
 				echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+				echo '<span class="hiddenAsCard displayTableRight hidden">';
+				echo ($eqLogic->getIsVisible() == 1) ? ' <i class="fas fa-eye" title="{{Equipement visible}}"></i>' : ' <i class="fas fa-eye-slash" title="{{Equipement non visible}}"></i>';
+				echo '</span>';
 				echo '</div>';
 			}
 			echo '</div>';
@@ -51,8 +55,8 @@ $eqLogics = eqLogic::byType($plugin->getId());
 	<div class="col-xs-12 eqLogic" style="display: none;">
 		<div class="input-group pull-right" style="display:inline-flex">
 			<span class="input-group-btn">
-				<a class="btn btn-default btn-sm eqLogicAction roundedLeft" data-action="configure"><i class="fas fa-cogs"></i> {{Configuration avancée}}
-				</a><a class="btn btn-default btn-sm eqLogicAction" data-action="copy"><i class="fas fa-copy"></i> {{Dupliquer}}
+				<a class="btn btn-sm btn-default eqLogicAction roundedLeft" data-action="configure"><i class="fas fa-cogs"></i><span class="hidden-xs"> {{Configuration avancée}}</span>
+				</a><a class="btn btn-sm btn-default eqLogicAction" data-action="copy"><i class="fas fa-copy"></i><span class="hidden-xs">  {{Dupliquer}}</span>
 				</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}
 				</a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
 			</span>
@@ -69,15 +73,15 @@ $eqLogics = eqLogic::byType($plugin->getId());
 						<div class="col-lg-6">
 							<legend><i class="fas fa-wrench"></i> {{Paramètres généraux}}</legend>
 							<div class="form-group">
-								<label class="col-sm-3 control-label">{{Nom du compteur Enedis}}</label>
-								<div class="col-sm-7">
-									<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-									<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement Enedis}}"/>
+								<label class="col-sm-4 control-label">{{Nom du compteur}}</label>
+								<div class="col-sm-6">
+									<input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" >
+									<input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom du compteur}}">
 								</div>
 							</div>
 							<div class="form-group">
-								<label class="col-sm-3 control-label" >{{Objet parent}}</label>
-								<div class="col-sm-7">
+								<label class="col-sm-4 control-label" >{{Objet parent}}</label>
+								<div class="col-sm-6">
 									<select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
 										<option value="">{{Aucun}}</option>
 										<?php	$options = '';
@@ -90,39 +94,39 @@ $eqLogics = eqLogic::byType($plugin->getId());
 								</div>
 							</div>
 							<div class="form-group">
-								<label class="col-sm-3 control-label">{{Catégorie}}</label>
-								<div class="col-sm-7">
+								<label class="col-sm-4 control-label">{{Catégorie}}</label>
+								<div class="col-sm-6">
 									<?php
 									foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
 										echo '<label class="checkbox-inline">';
-										echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+										echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" >' . $value['name'];
 										echo '</label>';
 									}
 									?>
 								</div>
 							</div>
 							<div class="form-group">
-								<label class="col-sm-3 control-label">{{Options}}</label>
-								<div class="col-sm-7">
-									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+								<label class="col-sm-4 control-label">{{Options}}</label>
+								<div class="col-sm-6">
+									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked>{{Activer}}</label>
+									<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked>{{Visible}}</label>
 								</div>
 							</div>
 
 							<legend><i class="fas fa-cogs"></i> {{Paramètres spécifiques}}</legend>
 							<div class="form-group">
-								<label class="col-sm-3 control-label">{{Point de livraison}}
-									<sup><i class="fas fa-question-circle tooltips" title="{{Indiquer le numéro de 14 caractères permettant d'identifier le point de livraison dont il faut récupérer les données}}"></i></sup>
+								<label class="col-sm-4 control-label">{{Point de livraison}}
+									<sup><i class="fas fa-question-circle tooltips" title="{{Indiquer le numéro d'identification du point de livraison (14 caractères)}}"></i></sup>
 								</label>
-								<div class="col-sm-7">
+								<div class="col-sm-6">
 									<input type="text" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="usage_point_id">
 								</div>
 							</div>
 							<div class="form-group">
-								<label class="col-sm-3 control-label"> {{Type de mesure}}
-									<sup><i class="fas fa-question-circle tooltips" title="{{Indiquer le type de mesure qu'il faut récupérer (consommation, production ou les deux)}}"></i></sup>
+								<label class="col-sm-4 control-label"> {{Type de mesure}}
+									<sup><i class="fas fa-question-circle tooltips" title="{{Indiquer le type de mesure à récupérer (consommation, production ou les deux)}}"></i></sup>
 								</label>
-								<div class="col-sm-7">
+								<div class="col-sm-6">
 									<select class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="measure_type">
 										<option value="consumption">{{Consommation}}</option>
 										<option value="production">{{Production}}</option>
@@ -135,22 +139,22 @@ $eqLogics = eqLogic::byType($plugin->getId());
 						<div class="col-lg-6">
 							<legend><i class="fas fa-tablet-alt"></i> {{Template de Widget}}</legend>
 							<div class="form-group">
-								<label class="col-sm-3 control-label">{{Activer}}
+								<label class="col-sm-4 control-label">{{Activer}}
 									<sup><i class="fas fa-question-circle tooltips" title="{{Cocher la case pour utiliser le template de widget représentant le compteur Linky}}"></i></sup>
 								</label>
-								<div class="col-sm-7">
-									<input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="widgetTemplate"/>
+								<div class="col-sm-6">
+									<input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="widgetTemplate">
 								</div>
 							</div>
 							<br>
 							<div class="form-group" id="templateParams">
-								<label class="col-sm-3 control-label">{{Couleur de fond}}
+								<label class="col-sm-4 control-label">{{Couleur de fond}}
 									<sup><i class="fas fa-question-circle tooltips" title="{{Sélectionner la couleur de fond du template de widget}}"></i></sup>
 								</label>
-								<div class="col-sm-3">
-									<input type="color" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="widgetBGColor"/>
+								<div class="col-sm-4">
+									<input type="color" class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="widgetBGColor">
 								</div>
-								<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="widgetTransparent"/>{{Transparent}}
+								<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="widgetTransparent">{{Transparent}}
 									<sup><i class="fas fa-question-circle tooltips" title="{{Cocher la case pour un fond transparent}}"></i></sup>
 								</label>
 							</div>
@@ -162,7 +166,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
 			</div>
 
 			<div role="tabpanel" class="tab-pane" id="commandtab">
-				<br/>
+				<br>
 				<div class="table-responsive">
 					<table id="table_cmd" class="table table-bordered table-condensed">
 						<thead>
@@ -180,8 +184,8 @@ $eqLogics = eqLogic::byType($plugin->getId());
 					</table>
 				</div>
 			</div>
-		</div>
 
+		</div>
 	</div>
 </div>
 
